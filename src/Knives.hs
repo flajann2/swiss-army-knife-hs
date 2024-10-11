9b0c404096ffd9b1176996aca7b4676594c9c2ba
El Toro Maestro <eltoro@atomlogik.de>
2024-10-11 18:36:18 -0500

    documentation

--- src/Knives.hs
+++ src/Knives.hs
@@ -120,13 +120,6 @@ knifeSleep opts = do
   _nn <-  readProcess "systemctl" ["suspend", "-i"] ""
   return ()
 
-
-
-
-
-
-
-
 knifeZfsClean :: ZfsCleanOptions -> IO ()
 knifeZfsClean opts = do
   putStrLn $ "Not Implemented Yet mode: " ++ show (notdefinedyet opts)
