698b34da96abfc4c25fffcaac3aff61fb1338cce
El Toro Maestro <eltoro@atomlogik.de>
2025-03-10 14:49:20 -0500

    lists status

--- lib/Knives/WireGuard.hs
+++ lib/Knives/WireGuard.hs
@@ -2,10 +2,12 @@
 module Knives.WireGuard where
 
 import CommandLine
+import System.IO
 import System.Process
 import System.Directory (getDirectoryContents)
 import System.FilePath (dropExtension, takeExtension)
 import Control.Monad (when)
+import Data.List
 
 knifeWireGuard :: WireGuardOptions -> IO ()
 knifeWireGuard WireGuardOptions { listWGs
@@ -14,7 +16,8 @@ knifeWireGuard WireGuardOptions { listWGs
                                 , reactivateWG}
   | checkMutualExclusivity = do
       wgs <- wgList
-      when listWGs $ print $ wgStatus wgs
+      ss  <- wgStatus wgs
+      when listWGs $ putStrLn $ formatStatus ss
       case (activateWG, deactivateWG, reactivateWG) of
         (Just vpn, False, False) -> activate vpn
         (Nothing, True, False)   -> deactivate
@@ -37,11 +40,24 @@ knifeWireGuard WireGuardOptions { listWGs
     deactivate   = undefined
     reactivate   = undefined
 
-    wgStatus :: [String] -> IO [(String, String)]
+    wgStatus :: [String] -> IO [(String, String, Bool)]
     wgStatus wgs = do
-      let quick = map (\w -> "wg-quick" ++ w) wgs
-      let cmd = ["systemctl", "is-active"] ++ quick
+      let quick = map (\w -> "wg-quick@" ++ w) wgs
+      let cmd = ["is-active"] ++ quick
+      (_, Just hout, _, _) <- createProcess (proc "systemctl" cmd) { std_out = CreatePipe }
+      out <- hGetContents hout
+      let stats = lines out
+      print out
+      return $ [(wg, sact, sact == "active") | (wg, sact) <- zip wgs stats  ]
 
+    formatStatus :: [(String, String, Bool)] -> String
+    formatStatus wss = intercalate "\n" [w ++ ": " ++ s |(w, s, _) <- wss]
 
-
- -- systemctl is-active wg-quick@nyc71 wg-quick@erf91
+-- systemctl is-active wg-quick@nyc71 wg-quick@erf91
+-- import System.Process
+-- 
+-- main :: IO ()
+-- main = do
+--     (_, Just hout, _, _) <- createProcess (proc "ls" ["-l"]) { std_out = CreatePipe }
+--     output <- hGetContents hout
+--     putStrLn output
